9fba2c0a2585b20a8c03ad4918036d554296dfa7
Alex Stark <539273+jalexstark@users.noreply.github.com>
2022-08-04 23:38:24 +0000

    Zebraix: Add basic tests with json I/O.

--- zebraix/src/drawable/src/drawable_graph/draw_tests.rs
+++ zebraix/src/drawable/src/drawable_graph/draw_tests.rs
@@ -12,15 +12,20 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+extern crate goldenfile;
+extern crate jaywalk_drawable;
+extern crate json5;
+
+use goldenfile::Mint;
+use std::fs::File;
+// use std::io::{BufRead, BufReader, Read, Write};
+use std::io::{Read, Write};
+// use std::path::Path;
+
+use jaywalk_drawable::drawable_graph::ZebraixGraph;
+
 pub fn add(a: i32, b: i32) -> i32 {
    a + b
-}
-
-// This is a really bad adding function, its purpose is to fail in this
-// example.
-#[allow(dead_code)]
-fn bad_add(a: i32, b: i32) -> i32 {
-   a - b
 }
 
 #[cfg(test)]
@@ -32,11 +37,48 @@ mod tests {
    fn test_add() {
       assert_eq!(add(1, 2), 3);
    }
+}
 
-   #[test]
-   fn test_bad_add() {
-      // This assert would fire and test will fail.
-      // Please note, that private functions can be tested too!
-      assert_eq!(bad_add(1, 2), 3);
+fn run_json_test(input_filename: &str, output_filename: &str, args: &[&str]) {
+   let mut string_args = Vec::new();
+
+   string_args.reserve(args.len());
+   for a in args.iter() {
+      string_args.push(a.to_string());
    }
+
+   let input_full_path = format!("src/drawable_graph/golden_inputs/{}", input_filename);
+
+   let mut in_text = String::new();
+   let mut inputfile = File::open(input_full_path).unwrap();
+   inputfile.read_to_string(&mut in_text).unwrap();
+
+   let deserialized = json5::from_str::<ZebraixGraph>(&in_text).unwrap();
+
+   assert!(deserialized.graph.nodes.len() == 3, "Incorrect number of nodes.");
+   // let inbound_serialized = zebraix_serialized::read_file(File::open(input_full_path).unwrap()).unwrap();
+
+   // assert!(
+   //     inbound_serialized.get_nodes()[0].get_label_text() == "Four legs",
+   //     "First entry not four legs"
+   // );
+
+   let mut mint = Mint::new("src/drawable_graph/golden_outputs");
+   let out_file = mint.new_goldenfile(output_filename).unwrap();
+
+   filtered_write_sample_to_file(out_file, &deserialized);
+}
+
+// Replace surface ID with generic ID, since this is changeable in tests.
+fn filtered_write_sample_to_file<W: Write>(mut out_stream: W, graph: &ZebraixGraph) {
+   // let intervening_writer = Vec::<u8>::new();
+   // let line_reader = std::io::BufReader::new(&**intervening_writer);
+
+   let out_data = json5::to_string(&graph).unwrap();
+   writeln!(out_stream, "{}", out_data).unwrap();
+}
+
+#[test]
+fn test_json_sphinx() {
+   run_json_test("sphinx.json", "sphinx_ranks.json", &["--label_with_ranks"]);
 }
