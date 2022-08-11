3df5e14052d136cc89b634995f815acf010d1777
Alex Stark <539273+jalexstark@users.noreply.github.com>
2022-08-11 15:48:50 +0000

    Zebraix: Added more basic SVG.

--- zebraix/src/drawable/src/drawable_graph/draw_tests.rs
+++ zebraix/src/drawable/src/drawable_graph/draw_tests.rs
@@ -22,6 +22,8 @@ use std::fs::File;
 use std::io::{Read, Write};
 // use std::path::Path;
 
+use jaywalk_drawable::drawable_graph::DCircle;
+use jaywalk_drawable::drawable_graph::DrawableGraph;
 use jaywalk_drawable::drawable_graph::ZebraixGraph;
 
 pub fn add(a: i32, b: i32) -> i32 {
@@ -39,7 +41,55 @@ mod tests {
    }
 }
 
-fn run_json_test(input_filename: &str, output_filename: &str, args: &[&str]) {
+fn run_graph_json_test(input_filename: &str, output_filename: &str, args: &[&str]) {
+   let mut string_args = Vec::new();
+
+   string_args.reserve(args.len());
+   for a in args.iter() {
+      string_args.push(a.to_string());
+   }
+
+   let input_full_path = format!("src/drawable_graph/golden_inputs/{}", input_filename);
+
+   let mut in_text = String::new();
+   let mut inputfile = File::open(input_full_path).unwrap();
+   inputfile.read_to_string(&mut in_text).unwrap();
+
+   let deserialized = json5::from_str::<DrawableGraph>(&in_text).unwrap();
+
+   // assert!(deserialized.graph.nodes.len() == 3, "Incorrect number of nodes.");
+
+   let mut mint = Mint::new("src/drawable_graph/golden_outputs");
+   let out_file = mint.new_goldenfile(output_filename).unwrap();
+
+   filtered_graph_write_sample_to_file(out_file, &deserialized);
+}
+
+fn run_circles_json_test(input_filename: &str, output_filename: &str, args: &[&str]) {
+   let mut string_args = Vec::new();
+
+   string_args.reserve(args.len());
+   for a in args.iter() {
+      string_args.push(a.to_string());
+   }
+
+   let input_full_path = format!("src/drawable_graph/golden_inputs/{}", input_filename);
+
+   let mut in_text = String::new();
+   let mut inputfile = File::open(input_full_path).unwrap();
+   inputfile.read_to_string(&mut in_text).unwrap();
+
+   let deserialized = json5::from_str::<Vec<DCircle>>(&in_text).unwrap();
+
+   // assert!(deserialized.graph.nodes.len() == 3, "Incorrect number of nodes.");
+
+   let mut mint = Mint::new("src/drawable_graph/golden_outputs");
+   let out_file = mint.new_goldenfile(output_filename).unwrap();
+
+   filtered_circle_write_sample_to_file(out_file, &deserialized);
+}
+
+fn run_old_json_test(input_filename: &str, output_filename: &str, args: &[&str]) {
    let mut string_args = Vec::new();
 
    string_args.reserve(args.len());
@@ -66,11 +116,29 @@ fn run_json_test(input_filename: &str, output_filename: &str, args: &[&str]) {
    let mut mint = Mint::new("src/drawable_graph/golden_outputs");
    let out_file = mint.new_goldenfile(output_filename).unwrap();
 
-   filtered_write_sample_to_file(out_file, &deserialized);
+   filtered_zebraix_write_sample_to_file(out_file, &deserialized);
 }
 
 // Replace surface ID with generic ID, since this is changeable in tests.
-fn filtered_write_sample_to_file<W: Write>(mut out_stream: W, graph: &ZebraixGraph) {
+fn filtered_zebraix_write_sample_to_file<W: Write>(mut out_stream: W, graph: &ZebraixGraph) {
+   // let intervening_writer = Vec::<u8>::new();
+   // let line_reader = std::io::BufReader::new(&**intervening_writer);
+
+   let out_data = json5::to_string(&graph).unwrap();
+   writeln!(out_stream, "{}", out_data).unwrap();
+}
+
+// Replace surface ID with generic ID, since this is changeable in tests.
+fn filtered_graph_write_sample_to_file<W: Write>(mut out_stream: W, graph: &DrawableGraph) {
+   // let intervening_writer = Vec::<u8>::new();
+   // let line_reader = std::io::BufReader::new(&**intervening_writer);
+
+   let out_data = json5::to_string(&graph).unwrap();
+   writeln!(out_stream, "{}", out_data).unwrap();
+}
+
+// Replace surface ID with generic ID, since this is changeable in tests.
+fn filtered_circle_write_sample_to_file<W: Write>(mut out_stream: W, graph: &Vec<DCircle>) {
    // let intervening_writer = Vec::<u8>::new();
    // let line_reader = std::io::BufReader::new(&**intervening_writer);
 
@@ -79,6 +147,16 @@ fn filtered_write_sample_to_file<W: Write>(mut out_stream: W, graph: &ZebraixGra
 }
 
 #[test]
-fn test_json_sphinx() {
-   run_json_test("sphinx.json", "sphinx_ranks.json", &["--label_with_ranks"]);
+fn test_old_json_sphinx() {
+   run_old_json_test("sphinx.json", "sphinx_ranks.json", &["--label_with_ranks"]);
+}
+
+#[test]
+fn test_graph_json_sphinx() {
+   run_graph_json_test("graphs_00.json", "graphs_00_converted.json", &["--label_with_ranks"]);
+}
+
+#[test]
+fn test_circles_json_sphinx() {
+   run_circles_json_test("circles_00.json", "circles_00_converted.json", &["--label_with_ranks"]);
 }
