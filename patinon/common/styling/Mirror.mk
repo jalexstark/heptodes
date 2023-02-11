2cbf2cf47a127e4e7c1a860c17707cb242a182a5
Alex Stark <539273+jalexstark@users.noreply.github.com>
2023-02-10 16:32:21 -0800

    Clean up styling, putting in common Patinon directory.
    Make is poorly designed for sub-make recursion, so this is a best-reasonable-effort.

---
+++ patinon/common/styling/Mirror.mk
@@ -0,0 +1,6 @@
+STYLE_FILES=crested.css defs-html.yaml defs-pdflatex.yaml header-html.html header-pdflatex.tex
+
+all:	$(STYLE_FILES)
+
+$(STYLE_FILES):	%:	../../../patinon/common/styling/%
+	cp $< .
